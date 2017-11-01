0b3d45ef7fc1f8ac46912e13b069451c42c1cb37
Chien Wen Chen <ai281918@gmail.com>
2017-11-01 20:51:57 +0800

    no message

--- FP.cpp
+++ FP.cpp
@@ -2,8 +2,7 @@
 
 void Initialize()
 {
-    root = new T_node();
-    ID_cnt.push_back(0);
+    memset(ID_cnt, 0, sizeof(ID_cnt));
 }
 
 bool cmp(int a, int b)
@@ -11,12 +10,16 @@ bool cmp(int a, int b)
     return ID_cnt[a] > ID_cnt[b];
 }
 
+bool cmp2(int a, int b)
+{
+    return ID_cnt[a] < ID_cnt[b];
+}
+
 int GetID(string name)
 {
     if(id_m[name] == 0){
         id_m[name] = ++id_count;
         name_m[id_count] = name;
-        ID_cnt.push_back(1);
     }
     return id_m[name];
 }
@@ -34,18 +37,32 @@ string GetName(int id)
     return name_m[id];
 }
 
-T_node* GetNodeChild(T_node *node, int id)
+void UpdateNodes(vector<vector<Node*> > &nodes, int id, Node *node)
 {
-    id--;
-    if(node -> children == NULL){
-        node -> children = (T_node**)malloc(24);
-        for(int i=0;i<id_count;++i){
-            (node -> children)[i] = NULL;
-        }
+    while(id >= nodes.size()){
+        vector<Node*> v;
+        nodes.push_back(v);
     }
+    nodes[id].push_back(node);
+}
+
+Node* GetFPNodeChild(Node *node, int id, vector<vector<Node*> > &nodes)
+{
     if((node -> children)[id] == NULL){
-        (node -> children)[id] = new T_node();
-        (node -> children)[id] -> value = id + 1;
+        (node -> children)[id] = new Node();
+        (node -> children)[id] -> value = id;
+        (node -> children)[id] -> parent = node;
+        UpdateNodes(nodes, id, (node -> children)[id]);
+    }
+    return (node -> children)[id];
+}
+
+Node* GetCandidateNodeChild(Node *node, int id)
+{
+    if((node -> children)[id] == NULL){
+        (node -> children)[id] = new Node();
+        (node -> children)[id] -> value = id;
+        (node -> children)[id] -> parent = node;
     }
     return (node -> children)[id];
 }
@@ -53,7 +70,7 @@ T_node* GetNodeChild(T_node *node, int id)
 void printV(vector<int> &v)
 {
     for(int i=0;i<v.size();++i){
-        cout << v[i] << " ";
+        cout << GetName(v[i]) << " ";
     }
     puts("");
 }
@@ -70,37 +87,208 @@ void printVV(vector<vector<int> > &v)
     puts("=============================================================");
 }
 
-void PrintFPTree(T_node* current)
+void PrintFPTree(Node* current)
 {
     cout << GetName(current -> value) << " " << current -> cnt << endl;
-    if((current -> children) != NULL){
-        for(int i=0;i<id_count;++i){
-            if((current -> children)[i] != NULL){
-                PrintFPTree((current -> children)[i]);
-            }
+    map<int, Node*>::iterator iter;
+
+    for(iter = (current -> children).begin(); iter != (current -> children).end(); iter++){
+        PrintFPTree(iter -> second);
+    }
+}
+
+void UpdateFPTree(vector<int> &set, Node* root, int ms, vector<vector<Node*> > &nodes, int n)
+{
+    Node* current = root;
+    for(int i=0;i<set.size();++i){
+        if(ID_cnt[set[i]] < ms) break;
+        current = GetFPNodeChild(current, set[i], nodes);
+        (current -> cnt) += n;
+    }
+}
+
+int GetCntFromTree(vector<int> &set, Node *root)
+{
+    Node *current = root;
+    for(int i=0;i<set.size();++i){
+        current = GetCandidateNodeChild(current, set[i]);
+    }
+    return current -> cnt;
+}
+
+void PrintRule(vector<int> &set, vector<int> &l, double mf, Node *root)
+{
+    if(double(GetCntFromTree(set, root)) / GetCntFromTree(l, root) < mf){
+        return;
+    }
+
+    vector<int> r;
+    for(int i=0, j=0;i<set.size();++i){
+        //cout << i << " " << j << endl;
+        if(j < l.size() && set[i] == l[j]){
+            j++;
+        }
+        else{
+            r.push_back(set[i]);
+        }
+    }
+
+    cout << "{";
+    for(int i=0;i<l.size();++i){
+        if(i) cout << ", ";
+        cout << GetName(l[i]);
+    }
+    cout << "} --> {";
+    for(int i=0;i<r.size();++i){
+        if(i) cout << ", ";
+        cout << GetName(r[i]);
+    }
+    cout << "} : ";
+    cout << double(GetCntFromTree(set, root)) / GetCntFromTree(l, root) << endl;;
+}
+
+void ComputeConf(vector<int> &set, vector<int> l, int cur, int cnt, int num, double mf, Node *root)
+{
+    if(cnt == num){
+        PrintRule(set, l, mf, root);
+    }
+    if(set.size() - cur < num - cnt) return;
+
+    for(int i=cur;i<set.size();++i){
+        l.push_back(set[i]);
+        ComputeConf(set, l, i+1, cnt+1, num, mf, root);
+        l.erase(l.end()-1);
+    }
+}
+
+void GenerateRule(vector<int> &candidate, int cnt, double mf, Node* root)
+{
+    vector<int> l;
+    // puts("=============================================================");
+    for(int i=0;i<candidate.size();++i){
+        cout << GetName(candidate[i]) << " ";
+    }
+    cout << ": " << cnt << endl;
+    // puts("-------------------------------------------------------------");
+    // for(int i=1;i<candidate.size();++i){
+    //     ComputeConf(candidate, l, 0, 0, i, mf, root);
+    // }
+    // puts("=============================================================");
+}
+
+void PrintCandidateTree(Node *root, int ms, float mf)
+{
+    queue<vector<int> > vq;
+    queue<Node*> nq;
+    vector<int> s;
+    Node *cur, *next;
+    vq.push(s);
+    nq.push(root);
+
+    while(!nq.empty()){
+        cur = nq.front();nq.pop();
+        s = vq.front();vq.pop();
+
+        GenerateRule(s, cur -> cnt, mf, root);
+
+        map<int, Node*>::iterator iter;
+        for(iter = (cur -> children).begin(); iter != (cur -> children).end(); iter++){
+            next = iter -> second;
+            s.push_back(next -> value);
+            nq.push(next);
+            vq.push(s);
+            s.erase(s.end()-1);
         }
     }
 }
 
-void UpdateFPTree(vector<int> set, T_node* root)
+void UpdateCandidateTree(Node *root, vector<int> s, int m)
 {
-    T_node* current = root;
-    for(int i=0;i<set.size();++i){
-        current = GetNodeChild(current, set[i]);
-        (current -> cnt)++;
+    Node *current = root;
+    sort(s.begin(), s.end());
+    for(int i=0;i<s.size();++i){
+        current = GetCandidateNodeChild(current, s[i]);
+    }
+    (current -> cnt) += m;
+}
+
+vector<int> FindPath(Node *node, int n)
+{
+    vector<int> path;
+    node = node -> parent;
+    while(node -> value){
+        path.push_back(node -> value);
+        ID_cnt[node -> value] += n;
+        node = node -> parent;
+    }
+    return path;
+}
+
+void FPTreeRetrieval(int id, Node *candidate_root, vector<vector<Node*> > &nodes, int ms, float mf, vector<int> &cond)
+{
+    vector<vector<int> > paths;
+    vector<vector<Node*> > sub_nodes;
+
+    memset(ID_cnt, 0, sizeof(ID_cnt));
+    Node* root = new Node();
+
+    for(int i=0;i<nodes[id].size();++i){
+        UpdateCandidateTree(candidate_root, cond, nodes[id][i] -> cnt);
+        paths.push_back(FindPath(nodes[id][i], nodes[id][i] -> cnt));
+    }
+    for(int i=0;i<paths.size();++i){
+        sort(paths[i].begin(), paths[i].end(), cmp);
+        UpdateFPTree(paths[i], root, ms, sub_nodes, nodes[id][i] -> cnt);
+    }
+
+    FPTreeMining(sub_nodes, ms, mf, cond, candidate_root);
+}
+
+void FPTreeMining(vector<vector<Node*> > &nodes, int ms, float mf, vector<int> &cond, Node *candidate_root)
+{
+    vector<int> items;
+    for(int i=1;i<=id_count;++i){
+        if(ID_cnt[i] >= ms){
+            items.push_back(i);
+        }
+    }
+    sort(items.begin(), items.end(), cmp2);
+
+    for(int i=0;i<items.size();++i){
+        cond.push_back(items[i]);
+        FPTreeRetrieval(items[i], candidate_root, nodes, ms, mf, cond);
+        cond.erase(cond.end()-1);
     }
 }
 
-int main()
+int main(int argc, char **argv)
 {
-	freopen("in.txt","r",stdin);
+    freopen("data.ntrans_1.nitems_0.1","r",stdin);
+    //freopen("in2.txt","r",stdin);
     Initialize();
 
-    int tid, pre = -1, cnt=0;
+    int tid, pre = -1, cnt=0, gg, ms;
+    float mf;
+    if(argc == 2){
+        ms = atoi(argv[1]);
+    }
+    else if(argc == 3){
+        ms = atoi(argv[1]);
+        mf = atof(argv[2]);
+    }
+    else{
+        ms = 5;
+        mf = 0.5;
+    }
+
     string name;
     vector<int> v;
+    Node* root = new Node();
+    Node *candidate_root = new Node();
+    vector<vector<int> > DB;
+    vector<vector<Node*> > nodes;
 
-    while(cin >> tid >> name){
+    while(cin >> tid >> gg >> name){
         if(tid != pre){
             if(pre != -1){
                 DB.push_back(v);
@@ -111,12 +299,17 @@ int main()
         v.push_back(GetID(name));
         UpdataCnt(name);
     }
+    DB.push_back(v);
     for(int i=0;i<DB.size();++i){
         sort(DB[i].begin(), DB[i].end(), cmp);
-        UpdateFPTree(DB[i], root);
+        UpdateFPTree(DB[i], root, ms, nodes, 1);
     }
-    printVV(DB);
-    PrintFPTree(root);
+
+    int t = GetTickCount();
+    vector<int> cond;
+    FPTreeMining(nodes, ms, mf, cond, candidate_root);
+    PrintCandidateTree(candidate_root, ms, mf);
+    cout << "Time : " << (double)(GetTickCount() - t) / 1000 << endl;
 
 	return 0;
 }
--- FP.h
+++ FP.h
@@ -9,17 +9,21 @@
 #include <climits>
 #include <stack>
 #include <map>
+#include <windows.h>
 using namespace std;
 
-struct T_node{
-    T_node **children = NULL;
+struct Node{
+    map<int, Node*> children;
+    Node *parent = NULL;
     int cnt = 0;
     int value = 0;
+    bool visit = false;
 };
 
-T_node* root;
-vector<vector<int> > DB;
-vector<int> ID_cnt;
+int ID_cnt[1000];
 map<string, int> id_m;
 map<int, string> name_m;
 int id_count = 0;
+
+void FPTreeRetrieval(int id, Node *candidate_root, vector<vector<Node*> > &nodes, int ms, float mf, vector<int> &cond);
+void FPTreeMining(vector<vector<Node*> > &nodes, int ms, float mf, vector<int> &cond, Node *candidate_root);
